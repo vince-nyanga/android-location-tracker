0e3d28189ba00f2ef03e061644f20b3bbd036876
Vincent <vince.nyanga@hotmail.com>
2017-08-09 11:00:56 +0200

    java docs

--- tracker/src/main/java/com/vinarah/locationtracker/LocationTrackerBuilder.java
+++ tracker/src/main/java/com/vinarah/locationtracker/LocationTrackerBuilder.java
@@ -8,7 +8,7 @@ public class LocationTrackerBuilder {
 
     private static final long DEFAULT_INTERVAL = 300000;
 
-    private static final int DEFAULT_DISPLACEMENT = 100;
+    private static final int DEFAULT_DISPLACEMENT = 500;
 
     private static final
     @LocationTracker.Priority
@@ -28,18 +28,38 @@ public class LocationTrackerBuilder {
         this.context = context;
     }
 
+    /**
+     * Sets the location update interval. Default value is 5 minutes.
+     */
     public LocationTrackerBuilder setInterval(long interval) {
         this.interval = interval;
         return this;
     }
 
+    /**
+     * Sets the minimum displacement in meters that should trigger location update. Default value
+     * is 500 meters.
+     *
+     * @param displacement
+     * @return
+     */
     public LocationTrackerBuilder setDisplacement(int displacement) {
         this.displacement = displacement;
         return this;
     }
 
-    public LocationTrackerBuilder setPriority(@LocationTracker.Priority int priority){
-        this.priority  = priority;
+    /**
+     * Sets the priority for the location request.
+     * <p>
+     * Priority can be:
+     * <li>PRIORITY_HIGH for precise location updates</li>
+     * <li>PRIORITY_POWER_SAVER for balancing between power and accuracy</li>
+     * <li>PRIORITY_LOW_POWER for low power usage</li>
+     * @param priority
+     * @return
+     */
+    public LocationTrackerBuilder setPriority(@LocationTracker.Priority int priority) {
+        this.priority = priority;
         return this;
     }
 
